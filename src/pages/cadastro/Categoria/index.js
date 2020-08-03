9d23a383290629518d6f19f88efd109af55f3ee6
Lucas Otaviani <lucas.otaviani@hotmail.com>
2020-08-02 22:33:04 -0300

    fixed the url

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -29,7 +29,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
+    const URL = window.location.hostname.includes('localhost') 
+      ? 'http://localhost:8080/categorias'
+      : 'https://aluraflix-lucas-otaviani.herokuapp.com/categorias';
 
     fetch(URL).then(async (serverResponse) => {
       const response = await serverResponse.json();
